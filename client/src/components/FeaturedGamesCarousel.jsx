c34725707694e8fa46bc750b804677467e241280
Kenny Cordero <geckob4i@gmail.com>
2023-07-13 12:37:54 -0500

    Set up home page carousel with links to correct page

--- client/src/components/FeaturedGamesCarousel.jsx
+++ client/src/components/FeaturedGamesCarousel.jsx
@@ -1,113 +1,202 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Carousel from 'react-bootstrap/Carousel';
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import Carousel from "react-bootstrap/Carousel";
+import { Link, useNavigate } from "react-router-dom";
+import { GameContext } from "../contexts/GameContext";
+
+//! Combine and add CSS. D.R.Y!
 
 function FeaturedGamesCarousel() {
-  const [featuredGames, setFeaturedGames] = useState([]);
-  const [hotGames, setHotGames] = useState([]);
-  const [lovedGames, setLovedGames] = useState([]);
+    const [featuredGames, setFeaturedGames] = useState([]);
+    const [hotGames, setHotGames] = useState([]);
+    const [lovedGames, setLovedGames] = useState([]);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const options = {
-      method: 'GET',
-      url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
-      headers: {
-        'X-RapidAPI-Key': '5353e51751msha2b28d9e3384746p1a9b44jsne8dbb6955924',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-      }
+    const { setGameData } = useContext(GameContext);
+
+    useEffect(() => {
+        const options = {
+            method: "GET",
+            url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+            headers: {
+                "X-RapidAPI-Key":
+                    "5353e51751msha2b28d9e3384746p1a9b44jsne8dbb6955924",
+                "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+            },
+        };
+
+        const fetchGames = async () => {
+            try {
+                const response = await axios.request(options);
+                const games = response.data;
+                const shuffledGames = games.sort(() => 0.5 - Math.random());
+                setFeaturedGames(shuffledGames.slice(0, 5));
+                setHotGames(shuffledGames.slice(5, 10));
+                setLovedGames(shuffledGames.slice(10, 15));
+            } catch (error) {
+                console.error(
+                    "There was an error retrieving the games:",
+                    error
+                );
+            }
+        };
+
+        fetchGames();
+    }, []);
+
+    const handleGameClick = (game) => {
+        setGameData(game);
+        navigate(`/game/${game.id}`);
     };
 
-    const fetchGames = async () => {
-      try {
-        const response = await axios.request(options);
-        const games = response.data;
-        const shuffledGames = games.sort(() => 0.5 - Math.random()); 
-        setFeaturedGames(shuffledGames.slice(0, 5));
-        setHotGames(shuffledGames.slice(5, 10));
-        setLovedGames(shuffledGames.slice(10, 15));
-      } catch (error) {
-        console.error('There was an error retrieving the games:', error);
-      }
-    };
-
-    fetchGames();
-  }, []);
-  return (
-    <div style={{display:'flex', justifyContent:'space-between'}}>
-        <div>
-            <h2>Featured Games</h2>
-            <div style={{position:'relative', height: '33rem', border:'2px solid red', minWidth:'20rem', maxWidth:'20rem'}}>
-              <Carousel interval={null}>
-                  {featuredGames.map((game, index) => (
-                      <Carousel.Item key={index}>
-                          <img
-                              className="d-block"
-                              src={game.thumbnail}
-                              alt={game.title}
-                              style={{width:'20rem', objectFit:'cover'}}
-                          />
-                          <div style={{marginTop:'15rem'}}>
-                            <Carousel.Caption>
-                                <h3>{game.title}</h3>
-                                <p>{game.short_description}</p>
-                            </Carousel.Caption>
-                          </div>
-                      </Carousel.Item>
-                  ))}
-              </Carousel>
+    return (
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>
+                <h2>Featured Games</h2>
+                <div
+                    style={{
+                        position: "relative",
+                        height: "33rem",
+                        border: "2px solid red",
+                        minWidth: "20rem",
+                        maxWidth: "20rem",
+                    }}
+                >
+                    <Carousel interval={null} indicators={false}>
+                        {featuredGames.map((game, index) => (
+                            <Carousel.Item
+                                key={index}
+                                onClick={() => handleGameClick(game)}
+                                style={{ cursor: "pointer" }}
+                            >
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center",
+                                        height: "100%",
+                                    }}
+                                >
+                                    <img
+                                        className="d-block"
+                                        src={game.thumbnail}
+                                        alt={game.title}
+                                        style={{
+                                            width: "100%",
+                                            objectFit: "cover",
+                                        }}
+                                    />
+                                    <div style={{ flexGrow: 1 }}></div>
+                                    <Carousel.Caption
+                                        style={{ position: "unset" }}
+                                    >
+                                        <h3>{game.title}</h3>
+                                        <p>{game.short_description}</p>
+                                    </Carousel.Caption>
+                                </div>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </div>
+            </div>
+            <div>
+                <h2>What's Hot</h2>
+                <div
+                    style={{
+                        position: "relative",
+                        height: "33rem",
+                        border: "2px solid red",
+                        minWidth: "20rem",
+                        maxWidth: "20rem",
+                    }}
+                >
+                    <Carousel interval={null} indicators={false}>
+                        {hotGames.map((game, index) => (
+                            <Carousel.Item
+                                key={index}
+                                onClick={() => handleGameClick(game)}
+                                style={{ cursor: "pointer" }}
+                            >
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center",
+                                        height: "100%",
+                                    }}
+                                >
+                                    <img
+                                        className="d-block"
+                                        src={game.thumbnail}
+                                        alt={game.title}
+                                        style={{
+                                            width: "100%",
+                                            objectFit: "cover",
+                                        }}
+                                    />
+                                    <div style={{ flexGrow: 1 }}></div>
+                                    <Carousel.Caption
+                                        style={{ position: "unset" }}
+                                    >
+                                        <h3>{game.title}</h3>
+                                        <p>{game.short_description}</p>
+                                    </Carousel.Caption>
+                                </div>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </div>
+            </div>
+            <div>
+                <h2>Gotta Love It</h2>
+                <div
+                    style={{
+                        position: "relative",
+                        height: "33rem",
+                        border: "2px solid red",
+                        minWidth: "20rem",
+                        maxWidth: "20rem",
+                    }}
+                >
+                    <Carousel interval={null} indicators={false}>
+                        {lovedGames.map((game, index) => (
+                            <Carousel.Item
+                                key={index}
+                                onClick={() => handleGameClick(game)}
+                                style={{ cursor: "pointer" }}
+                            >
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center",
+                                        height: "100%",
+                                    }}
+                                >
+                                    <img
+                                        className="d-block"
+                                        src={game.thumbnail}
+                                        alt={game.title}
+                                        style={{
+                                            width: "100%",
+                                            objectFit: "cover",
+                                        }}
+                                    />
+                                    <div style={{ flexGrow: 1 }}></div>
+                                    <Carousel.Caption
+                                        style={{ position: "unset" }}
+                                    >
+                                        <h3>{game.title}</h3>
+                                        <p>{game.short_description}</p>
+                                    </Carousel.Caption>
+                                </div>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </div>
             </div>
         </div>
-        <div>
-            <h2>What's Hot</h2>
-            <div style={{position:'relative', height: '33rem', border:'2px solid red', minWidth:'20rem', maxWidth:'20rem'}}>
-              <Carousel interval={null}>
-                  {hotGames.map((game, index) => (
-                      <Carousel.Item key={index}>
-                          <img
-                              className="d-block"
-                              src={game.thumbnail}
-                              alt={game.title}
-                              style={{maxWidth:'100%', objectFit:'cover'}}
-                          />
-                          <div style={{marginTop:'15rem'}}>
-                            <Carousel.Caption>
-                                <h3>{game.title}</h3>
-                                <p>{game.short_description}</p>
-                            </Carousel.Caption>
-                          </div>
-                      </Carousel.Item>
-                  ))}
-              </Carousel>
-            </div>
-        </div>
-        <div>
-    <h2>Gotta Love It</h2>
-    <div style={{position:'relative', height: '33rem', border:'2px solid red', minWidth:'20rem', maxWidth:'20rem'}}>
-      <Carousel interval={null}>
-          {lovedGames.map((game, index) => (
-              <Carousel.Item key={index} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                  <img
-                      className="d-block"
-                      src={game.thumbnail}
-                      alt={game.title}
-                      style={{width:'100%', objectFit:'cover'}}
-                  />
-                  <div style={{height: '5rem'}}></div>
-                  <Carousel.Caption style={{border:'2px solid green', position: 'unset'}}>
-                      <h3>{game.title}</h3>
-                      <p>{game.short_description}</p>
-                  </Carousel.Caption>
-              </Carousel.Item>
-          ))}
-      </Carousel>
-    </div>
-</div>
-
-    </div>
-);
-
-
-
+    );
 }
 
 export default FeaturedGamesCarousel;
