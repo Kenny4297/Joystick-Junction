a34e9d3e491e0c301df493132814203ad80fe63c
Kenny Cordero <geckob4i@gmail.com>
2023-07-26 12:28:07 -0500

    Deployment Testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,23 +3,23 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-// const sequelize = process.env.MYSQL_URL
-//   ? new Sequelize(process.env.MYSQL_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306,
-//       logging: console.log
-//     });
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.MYSQL_URL
+  ? new Sequelize(process.env.MYSQL_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
       logging: console.log
-  });
+    });
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//       logging: console.log
+//   });
 
 
 module.exports = sequelize;
