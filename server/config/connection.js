8c36e9c67bbf474ecd586f8dc0c313114c5d4669
Kenny Cordero <geckob4i@gmail.com>
2023-07-26 12:39:52 -0500

    Deployment testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-const sequelize = process.env.MYSQL_URL
-  ? new Sequelize(process.env.MYSQL_URL)
+const sequelize = process.env.RAILWAY_MYSQL_URL
+  ? new Sequelize(process.env.RAILWAY_MYSQL_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
@@ -12,14 +12,6 @@ const sequelize = process.env.MYSQL_URL
       logging: console.log
     });
 
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306,
-//       logging: console.log
-//   });
 
 
 module.exports = sequelize;
