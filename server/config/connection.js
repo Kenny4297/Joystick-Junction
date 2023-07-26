b8efca0b14623ee956986da2bbc824193595fc7b
Kenny Cordero <geckob4i@gmail.com>
2023-07-26 10:56:37 -0500

    Add JawsDB data to config file

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,14 +3,24 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-const sequelize = process.env.MYSQL_URL
-  ? new Sequelize(process.env.MYSQL_URL)
+// const sequelize = process.env.MYSQL_URL
+//   ? new Sequelize(process.env.MYSQL_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//       logging: false
+//     });
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
       logging: false
-    });
+  });
+
 
 module.exports = sequelize;
 
