e5ebf23564d324df6a0969b7fb97447823f502f5
Kenny Cordero <geckob4i@gmail.com>
2023-07-26 17:28:59 -0500

    Deployment Testing

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -25,7 +25,7 @@ Comment.init(
             allowNull: false,
             onDelete: "cascade",
             references: {
-                model: "post",
+                model: "Post",
                 key: "id",
             },
         },
